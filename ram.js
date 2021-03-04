3baf696f0cc8fa1b63ac67f7d1acca6f917a28bc
ram chapagai <chapagairamm@icloud.com>
2021-03-03 21:25:12 -0600

    Added visitors greeting

--- ram.js
+++ ram.js
@@ -11,7 +11,28 @@ window.setInterval(function() {
         document.querySelector('#pLang').innerHTML = programmingLanguages[Math.floor(Math.random() * 7)];
   
 }, 2000);
+
+greetVisitor();
    
 })  
+
+function greetVisitor(){
+   var myDate = new Date();
+    var hrs = myDate.getHours();
+
+    var greet;
+
+    if (hrs < 12)
+        greet = 'Good Morning';
+    else if (hrs >= 12 && hrs <= 17)
+        greet = 'Good Afternoon';
+    else if (hrs >= 17 && hrs <= 24)
+        greet = 'Good Evening';
+
+    document.getElementById('greet').innerHTML = greet + '!';
+
+}
+
+   
 
 
